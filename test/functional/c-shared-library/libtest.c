435d4ab04ce4c1e1e5505f3dc28a515b969e5665
Raphaël Londeix <raphael.londeix@gmail.com>
2015-02-22 17:12:31 +0100

    test.functional.c-shared-library: Use portable export attribute.

--- test/functional/c-shared-library/libtest.c
+++ test/functional/c-shared-library/libtest.c
@@ -1,5 +1,9 @@
-
-__declspec(dllexport) int libtest_main()
+#ifdef __GNUC__
+ #define DLL_PUBLIC __attribute__ ((dllexport))
+#else
+ #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+#endif
+DLL_PUBLIC int libtest_main()
 {
 	return 0;
 }
