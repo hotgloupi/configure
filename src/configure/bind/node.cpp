649bf3c028b4d7b972ae7af761a20763b1334bdb
Raphaël Londeix <raphael.londeix@gmail.com>
2015-02-26 08:43:59 +0100

    Bind node properties accessor.

--- src/configure/bind/node.cpp
+++ src/configure/bind/node.cpp
@@ -1,6 +1,8 @@
-#include <configure/bind.hpp>
+#include "environ_utils.hpp"
 
+#include <configure/bind.hpp>
 #include <configure/Node.hpp>
+#include <configure/PropertyMap.hpp>
 #include <configure/lua/State.hpp>
 #include <configure/lua/Type.hpp>
 
@@ -9,6 +11,27 @@
 namespace fs = boost::filesystem;
 
 namespace configure {
+
+	static int Node_property(lua_State* state)
+	{
+		NodePtr& self = lua::Converter<NodePtr>::extract(state, 1);
+		Environ& env = self->properties().values();
+		return utils::env_get(state, env);
+	}
+
+	static int Node_set_property(lua_State* state)
+	{
+		NodePtr& self = lua::Converter<NodePtr>::extract(state, 1);
+		Environ& env = self->properties().values();
+		return utils::env_set(state, env);
+	}
+
+	static int Node_set_property_default(lua_State* state)
+	{
+		NodePtr& self = lua::Converter<NodePtr>::extract(state, 1);
+		Environ& env = self->properties().values();
+		return utils::env_set_default(state, env);
+	}
 
 	void bind_node(lua::State& state)
 	{
@@ -38,6 +61,23 @@ namespace configure {
 			// @treturn Path the relative path
 			// @function Node:relative_path
 			.def("relative_path", &Node::relative_path)
+
+			/// Retrieve a Node property
+			// @string name The property name
+			// @treturn string|Path|boolean|nil
+			.def("property", &Node_property)
+
+			/// Set a Node property
+			// @string name The property name
+			// @tparam string|Path|boolean|nil the value to set
+			// @treturn string|Path|boolean|nil
+			.def("set_property", &Node_set_property)
+
+			/// Set a Node property default value
+			// @string name The property name
+			// @tparam string|Path|boolean|nil the default value to set
+			// @treturn string|Path|boolean|nil
+			.def("set_property_default", &Node_set_property_default)
 		;
 	}
 
