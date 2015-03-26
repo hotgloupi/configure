0da7c3046533b8025f50bba3ddba8d27afadbce0
Raphaël Londeix <raphael.londeix@8cube.io>
2015-03-26 20:51:15 +0100

    Fix process test on windows.

--- test/unit/process.cpp
+++ test/unit/process.cpp
@@ -1,22 +1,20 @@
 #include <configure/Process.hpp>
+#include <configure/error.hpp>
 
 #include <iostream>
 
 using configure::Process;
 
 #ifdef _WIN32
-# define LS_COMMAND "dir"
+# define LS_COMMAND {"cmd", "/k", "dir"}
 #else
-# define LS_COMMAND "ls"
+# define LS_COMMAND {"ls"}
 #endif
 
 BOOST_AUTO_TEST_CASE(ls)
 {
-	for (int i = 0; i < 1000; i++)
-	{
-		auto out = Process::check_output({"ls"});
-		BOOST_CHECK(!out.empty());
-	}
+	auto out = Process::check_output(LS_COMMAND);
+	BOOST_CHECK(!out.empty());
 }
 
 BOOST_AUTO_TEST_CASE(empty_output)
