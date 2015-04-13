2a4015fba4556b75f4846e5212ad4b80ad65e127
Raphaël Londeix <raphael.londeix@infinit.io>
2015-04-13 09:06:12 +0200

    Bind Process.Stream and accept options in Process::check_output binding.

--- src/configure/bind/process.cpp
+++ src/configure/bind/process.cpp
@@ -30,21 +30,69 @@ namespace configure {
 				command.push_back(lua_tostring(state, -1));
 			else
 				CONFIGURE_THROW(
-					error::LuaError("Expected node, path or string array, got '" + std::string(luaL_tolstring(state, -1, nullptr)) + "'")
+					error::LuaError(
+						"Expected node, path or string, got '" +
+						std::string(luaL_tolstring(state, -1, nullptr)) + "'"
+					)
 				);
 		}
-		lua::Converter<std::string>::push(state, Process::check_output(command));
+		Process::Options options;
+		options.stdout_ = Process::Stream::PIPE;
+		options.stderr_ = Process::Stream::DEVNULL;
+		bool ignore_errors = false;
+		if (lua_istable(state, 3))
+		{
+			lua_pushnil(state);
+			while (lua_next(state, 3))
+			{
+				std::string key = lua::Converter<std::string>::extract(state, -2);
+				if (key == "stdin")
+					options.stdin_ = static_cast<Process::Stream>(lua::Converter<int>::extract(state, -1));
+				else if (key == "stdout")
+					options.stdout_ = static_cast<Process::Stream>(lua::Converter<int>::extract(state, -1));
+				else if (key == "stderr")
+					options.stderr_ = static_cast<Process::Stream>(lua::Converter<int>::extract(state, -1));
+				else if (key == "ignore_errors")
+					ignore_errors = lua::Converter<bool>::extract(state, -1);
+				else
+					CONFIGURE_THROW(
+					    error::InvalidArgument("Unknown argument '" + key + "'")
+					);
+				lua_pop(state, 1);
+			}
+			lua_pop(state, 1);
+		}
+		lua::Converter<std::string>::push(
+			state,
+			Process::check_output(command, options, ignore_errors)
+		);
 		return 1;
 	}
 
 	void bind_process(lua::State& state)
 	{
-		lua::Type<Process>(state, "Process")
+		lua::Type<Process> type(state, "Process");
+		type
 			/// Retreive a command output
 			// @function Process::check_output
 			// @treturn string Command output
 			.def("check_output", Process_check_output)
 		;
+
+#define ENUM_VALUE(T, key) \
+		lua_pushunsigned(\
+			state.ptr(), \
+			static_cast<unsigned>(Process::T::key)\
+		); \
+		lua_setfield(state.ptr(), -2, #key); \
+/**/
+		lua_newtable(state.ptr());
+		ENUM_VALUE(Stream, STDIN);
+		ENUM_VALUE(Stream, STDOUT);
+		ENUM_VALUE(Stream, STDERR);
+		ENUM_VALUE(Stream, PIPE);
+		ENUM_VALUE(Stream, DEVNULL);
+		lua_setfield(state.ptr(), -2, "Stream");
 	}
 
 }
