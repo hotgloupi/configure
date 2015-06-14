56485cd7da4600721eb0eec36fd6824271b1aedd
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-06-14 15:40:36 +0200

    lang.c: Libraries have runtime files attribute.

--- src/lib/configure/lang/c/Library.lua
+++ src/lib/configure/lang/c/Library.lua
@@ -8,7 +8,8 @@ return {
 	-- @param args.name The name of the library
 	-- @param[opt] args.system Whether or not the library is available at system level
 	-- @param[opt] args.include_directories Include directories
-	-- @param[opt] args.files Node or path to files to link against
+	-- @param[opt] args.files list of Node or path to files to link against
+	-- @param[opt] args.runtime_files list of Node or path to files that are needed at runtime
 	-- @param[opt] args.defines List of defines
 	new = function(self, args)
 		assert(args.name)
@@ -17,6 +18,7 @@ return {
 		o.system = args.system or false
 		o.include_directories = args.include_directories or {}
 		o.files = args.files or {}
+		o.runtime_files = args.runtime_files or {}
 		o.defines = args.defines or {}
 		o.kind = args.kind
 		assert(o.kind == nil or o.kind == 'static' or o.kind == 'shared')
